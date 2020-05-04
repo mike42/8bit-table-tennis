1147b93079b7e958e6c7d3fee4acc40d6d32d039
Michael Billington <michael.billington@gmail.com>
2020-05-04 22:10:25 +1000

    change game colors

--- table_tennis.s
+++ table_tennis.s
@@ -398,11 +398,12 @@ example_palette:
 .byte $0F,$15,$26,$37 ; bg0 purple/pink
 .byte $0F,$09,$19,$29 ; bg1 green
 .byte $0F,$01,$11,$21 ; bg2 blue
-.byte $0F,$00,$10,$30 ; bg3 greyscale
-.byte $0F,$18,$28,$38 ; sp0 yellow
-.byte $0F,$14,$24,$34 ; sp1 purple
-.byte $0F,$1B,$2B,$3B ; sp2 teal
-.byte $0F,$12,$22,$32 ; sp3 marine
+.byte $0F,$18,$28,$38 ; bg3 yellow
+.byte $0F,$12,$22,$32 ; sp0 marine
+.byte $0F,$00,$10,$30 ; sp1 greyscale
+.byte $0F,$14,$24,$34 ; sp2 purple
+.byte $0F,$1B,$2B,$3B ; sp3 teal
+
 
 .segment "ZEROPAGE"
 player1_x: .res 1
@@ -530,6 +531,7 @@ apply_physics: ; yeah 'physics'
       sta ball_state
     :
   :
+  clc
   lda ball_state
   and #BALL_UP
     beq :+
@@ -676,6 +678,7 @@ draw_game_sprites:
   sta oam+(5*4)+2
   sta oam+(6*4)+2
   sta oam+(7*4)+2
+  lda #%00000001 ; greyscale
   sta oam+(8*4)+2
   ; x position
   lda player1_x
